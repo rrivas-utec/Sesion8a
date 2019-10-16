b0ed966ec7b7b5d2326273e5289be778881b6973
rrivas-utec <rrivas@utec.edu.pe>
2019-10-16 10:48:05 -0500

    First Commit

---
+++ cilindro.cpp
@@ -0,0 +1,5 @@
+//
+// Created by utec on 16/10/19.
+//
+
+#include "cilindro.h"
---
+++ cilindro.h
@@ -0,0 +1,14 @@
+//
+// Created by utec on 16/10/19.
+//
+
+#ifndef SESION8A_CILINDRO_H
+#define SESION8A_CILINDRO_H
+
+
+class cilindro_t {
+
+};
+
+
+#endif //SESION8A_CILINDRO_H
